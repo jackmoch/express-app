c8a524ee8de3cedc7fb38dd39c425d437fcf192e
Jack Mocherman <mochermanjack@gmail.com>
2016-09-14 10:51:26 -0500

    finished post function for order in index.js and added order model to model folder

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const { Router } = require('express')
 const Contact = require('../models/contact')
+const Order = require('../models/order')
 const router = Router()
 
 router.get('/', (req, res) => {
@@ -31,6 +32,13 @@ router.get('/order', (req, res) => {
 	res.render('order', {
 		page: 'Order'
 	})
+})
+
+router.post('/order', (req, res) => {
+	const msg = new Order(req.body)
+	msg.save()
+		.then(() => res.redirect('/'))
+		.catch(() => res.send('BAD'))
 })
 
 module.exports = router
