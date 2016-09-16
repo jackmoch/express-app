75f03131c8b7d48597b9a88e4407c5581dbe7dc8
Jack Mocherman <mochermanjack@gmail.com>
2016-09-16 10:59:01 -0500

    save

--- database.js
+++ database.js
@@ -3,7 +3,6 @@
 const mongoose = require('mongoose')
 
 const MONGODB_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/pugpizza'
-// const MONGODB_URL = 'mongodb://pugspizza:jmoran@ds033116.mlab.com:33116/pugpizza'
 
 mongoose.Promise = Promise
 
