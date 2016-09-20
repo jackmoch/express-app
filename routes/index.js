b3f77fd581f314dbbd01ea0e59f2dc009975c9c8
Jack Mocherman <mochermanjack@gmail.com>
2016-09-20 10:09:54 -0500

    added register and login capabilities

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,81 @@
 'use strict'
 
 const { Router } = require('express')
+const bcrypt = require('bcrypt')
+
+const router = Router()
+
 const Contact = require('../models/contact')
 const Order = require('../models/order')
 const Size = require('../models/size')
 const Topping = require('../models/topping')
-const router = Router()
+const User = require('../models/user')
+
 
 router.get('/', (req, res) => {
   res.render('home')
+})
+
+router.get('/login', (req, res) => {
+	res.render('login')
+})
+
+router.get('/register', (req, res) => {
+	res.render('register')
+})
+
+router.post('/login', ({ body: { email, password }}, res, err) => {
+	User.findOne({ email })
+		.then(user => {
+			if(user) {
+				return new Promise((resolve, reject) => {
+					bcrypt.compare(password, user.password, (err, matches) => {
+						if (err) {
+							reject(err) 
+						} else {
+							resolve(matches)
+						}
+					})
+				})
+			} else {
+				res.render('login', { msg: 'Email does not exist in our system' })
+			}
+		})
+		.then(matches => {
+			if (matches) {
+				res.redirect('/')
+			} else {
+				res.render('login', { msg: 'Password does not match'})
+			}
+		})
+		.catch(err)
+})
+
+router.post('/register', ({ body: { email, password, passwordConfirmation } }, res, err) => {
+	if (password === passwordConfirmation) {
+		User.findOne({email})
+			.then(user => {
+				if (user) {
+					res.render('register', { msg: 'Email is already registered' })
+				} else {
+					return new Promise((resolve, reject) => {
+		 				bcrypt.hash(password, 15, (err, hash) => {
+	 						if (err) {
+	 							reject(err)
+	 						} else {
+	 							resolve(hash)
+	 						}
+						})
+					})
+				}
+			})
+			.then(hash => User.create({ email, password: hash}))
+			.then(() => res.redirect('/login'))
+			.catch(err) 
+	} 
+	else {
+		res.render('register', { msg: 'Password & password confirmation do not match' })
+	}
 })
 
 router.get('/about', (req, res) => {
@@ -23,11 +90,11 @@ router.get('/contact', (req, res) => {
   })
 })
 
-router.post('/contact', (req, res, error) => {
+router.post('/contact', (req, res, err) => {
 	Contact
 	.create(req.body)
 	.then(() => res.redirect('/'))
-	.catch(error)
+	.catch(err)
 })
 
 router.get('/order', (req, res) => {
@@ -38,11 +105,11 @@ router.get('/order', (req, res) => {
 	.then(([sizes, toppings]) => res.render('order',{page: 'Order', sizes, toppings}))
 })
 
-router.post('/order', (req, res, error) => {
+router.post('/order', (req, res, err) => {
 	Order
 	.create(req.body)
 	.then(() => res.redirect('/'))
-	.catch(error)	
+	.catch(err)	
 })
 
 module.exports = router
